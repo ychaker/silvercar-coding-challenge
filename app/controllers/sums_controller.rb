362906c2d6cdc5ce0dfaa057cce95269025c5c8c
Youssef Chaker <youssef@bearandgiraffe.com>
2020-03-02 15:55:43 -0600

    Adds comment about view table benchmark.

--- app/controllers/sums_controller.rb
+++ app/controllers/sums_controller.rb
@@ -1,5 +1,13 @@
 class SumsController < ApplicationController
   def show
+    # Investigated added a view table that only has the metrics from
+    # the last hour to make the query more performant
+    # but realized that because it's a time based query then we'd have
+    # to manually refresh the view table before making the query, like so:
+    #   RecentMetric.refresh
+    #   RecentMetric.for_key('active_visitors').pluck_value
+    # which would then take more time (based on a few benchmarks I ran)
+    # then calling this query below.
     values = Metric.for_key(params[:key]).last_hour.pluck_value
 
     render json: { value: values.sum }, status: :ok
